cac138c546d2474c6c40aec121c3e4cdf83904e2
Samuel Chen <samuel@Samuels-MacBook-Pro-2.local>
2022-09-26 17:30:18 -0700

    Add Modbus Slave to Grid Meter

--- io.openems.edge.simulator/src/io/openems/edge/simulator/meter/grid/acting/GridMeter.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/meter/grid/acting/GridMeter.java
@@ -1,3 +1,4 @@
+
 package io.openems.edge.simulator.meter.grid.acting;
 
 import java.io.IOException;
@@ -19,6 +20,13 @@ import org.osgi.service.event.EventHandler;
 import org.osgi.service.event.propertytypes.EventTopics;
 import org.osgi.service.metatype.annotations.Designate;
 
+import com.ghgande.j2mod.modbus.ModbusException;
+import com.ghgande.j2mod.modbus.procimg.ProcessImage;
+import com.ghgande.j2mod.modbus.procimg.SimpleProcessImage;
+import com.ghgande.j2mod.modbus.procimg.SimpleRegister;
+import com.ghgande.j2mod.modbus.slave.ModbusSlave;
+import com.ghgande.j2mod.modbus.slave.ModbusSlaveFactory;
+
 import io.openems.common.channel.Unit;
 import io.openems.common.types.ChannelAddress;
 import io.openems.common.types.OpenemsType;
@@ -35,6 +43,7 @@ import io.openems.edge.simulator.datasource.api.SimulatorDatasource;
 import io.openems.edge.timedata.api.Timedata;
 import io.openems.edge.timedata.api.TimedataProvider;
 import io.openems.edge.timedata.api.utils.CalculateEnergyFromPower;
+
 
 @Designate(ocd = Config.class, factory = true)
 @Component(name = "Simulator.GridMeter.Acting", //
@@ -66,9 +75,60 @@ public class GridMeter extends AbstractOpenemsComponent
 		}
 	}
 
+
+	private ProcessImage createProcessImage() {
+
+		int UNIT_ID = 1;
+
+		var image = new SimpleProcessImage(UNIT_ID);
+		image.addRegister(1000, new SimpleRegister(500));
+		image.addRegister(1001, new SimpleRegister(100));
+		image.addRegister(2000, new SimpleRegister(123));
+		return image;
+	}
+	
+	private ProcessImage createProcessImagePower(final int Power) {
+
+		int UNIT_ID = 1;
+
+		var image = new SimpleProcessImage(UNIT_ID);
+		image.addRegister(1002, new SimpleRegister(Power));
+		return image;
+	}
+
+	
+	private ModbusSlave slave; 
+
+	
+	public void start() throws ModbusException {
+		
+		int PORT = 8502;
+		int CONCURRENT_CONNECTIONS = 5;
+		int UNIT_ID = 1;
+		
+		// Start Modbus/TCP slave
+		var image = createProcessImage();
+		
+		this.slave = ModbusSlaveFactory.createTCPSlave(PORT, CONCURRENT_CONNECTIONS);
+		this.slave.addProcessImage(1, image);
+		this.slave.open();
+
+		System.out.println("Modbus-Slave Simulator started. Port [" + PORT + "] Unit-ID [" + UNIT_ID + "]");
+
+		// Stop Modbus/TCP slave on shutdown
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				ModbusSlaveFactory.close();
+				System.out.println("Modbus-Slave Simulator stopped.");
+			}
+		});
+
+	}
+
 	@Reference
 	protected ConfigurationAdmin cm;
-
+	
 	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
 	protected SimulatorDatasource datasource;
 
@@ -84,13 +144,16 @@ public class GridMeter extends AbstractOpenemsComponent
 			SymmetricMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY);
 
 	@Activate
-	void activate(ComponentContext context, Config config) throws IOException {
+	void activate(ComponentContext context, Config config) throws IOException, ModbusException {
 		super.activate(context, config.id(), config.alias(), config.enabled());
 
 		// update filter for 'datasource'
 		if (OpenemsComponent.updateReferenceFilter(this.cm, this.servicePid(), "datasource", config.datasource_id())) {
 			return;
 		}
+		
+		this.start();
+		
 	}
 
 	@Override
@@ -149,6 +212,15 @@ public class GridMeter extends AbstractOpenemsComponent
 		this._setActivePowerL1(activePowerByThree);
 		this._setActivePowerL2(activePowerByThree);
 		this._setActivePowerL3(activePowerByThree);
+		
+		int UNIT_ID = 1;
+		var image = new SimpleProcessImage(UNIT_ID);
+		image.addRegister(1000, new SimpleRegister(activePower));
+		image.addRegister(1001, new SimpleRegister(activePowerByThree));
+		image.addRegister(1002, new SimpleRegister(activePowerByThree));
+		image.addRegister(1003, new SimpleRegister(activePowerByThree));
+		this.slave.addProcessImage(1, image);
+		
 	}
 
 	@Override
